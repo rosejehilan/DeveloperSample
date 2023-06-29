74a6a4a5a28d9dda458b4fb67dbd7a3874a58352
Rose Jehilan John <rosejehilan@gmail.com>
2023-06-28 21:56:50 -0500

    Update LoginForm.js

--- Frontend/login_app/src/LoginForm.js
+++ Frontend/login_app/src/LoginForm.js
@@ -1,25 +1,34 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import './LoginForm.css';
 
-const LoginForm = (props) => {
-	const [userName, setUsername] = useState("");
+const LoginForm = ({onSubmit}) => {
+	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
-	const handleSubmit = (event) =>{
-		event.preventDefault();
-
-		props.onSubmit({
-			login: userName,
-			password: password,
-		});
-	}
+  
+	const handleSubmit = (e) => {
+	  e.preventDefault();
+	  onSubmit(username, password);
+	  setUsername("");
+	  setPassword("");
+	};
 
 	return (
-		<form className="form">
+		<form className="form"> 
 			<h1>Login</h1>
 			<label htmlFor="name">Name</label>
-			<input type="text" id="name" value={userName} onChange={(e)=>setUsername(e.target.value) } required />
+			<input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value) } required
+        />
 			<label htmlFor="password">Password</label>
-			<input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+			<input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} required
+        />
 			<button type="submit" onClick={handleSubmit}>Continue</button>
 		</form>
 	)
